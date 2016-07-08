80c4906b287e7f25b8938cb4b0b2b338a2e5fafa
Phoebe <yushih7533@gmail.com>
2016-07-08 12:30:46 +0000

    fix match content

--- route/match/index.js
+++ route/match/index.js
@@ -23,14 +23,14 @@ exports.process = function(recipientId, payload){
   routes[payload.action].process(recipientId, payload.response);
 }
 
-exports.save = function(recipientId, field, value, fn){
+exports.save = function(recipientId, key, field, value, fn){
     var updates = {};
     var updated_time = Date.now();
     updates['/userprefer/{user_id}/{field}'.replace('{user_id}', recipientId).replace('{field}', field)] = value;
     updates['/userprefer/{user_id}/updated_time'.replace('{user_id}', recipientId)] = updated_time;
     db.update(updates);
     UserPrefer.find(recipientId, function(userPrefer){
-        fn(userPrefer);
+        fn(recipientId, value, userPrefer);
     });
 }
 
